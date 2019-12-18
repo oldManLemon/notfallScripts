9cd7cbe3444f349c92582b2524e0d7f042ca2330
Hase <ahase@intern.ahs-de.com>
2019-12-18 15:09:36 +0100

    basic dhcp backup function

--- args.ps1
+++ args.ps1
@@ -1,39 +1,44 @@
-param([Parameter(mandatory=$true)]
-    [string]$Station,
-    [switch]$Reverse
+# param([Parameter(mandatory=$true)]
+#     [string]$Station,
+#     [switch]$Reverse
 
-    )
+#     )
 
-#Test args
-if($station -eq ''){
-    write-host "No Station Provided `n Stop"
-    break
-}
+# #Test args
+# if($station -eq ''){
+#     write-host "No Station Provided `n Stop"
+#     break
+# }
 
 
 
-#Flags
-$MucGO  = $false	
-$HamGO = $false	
-$HqGO = $false	
-$BreGO = $false	
-$CgnGO = $false	
-$BerGO = $false	
-$HajGO = $false	
-$FraGO = $false	
-$AllGO = $false	
+# #Flags
+# $MucGO  = $false	
+# $HamGO = $false	
+# $HqGO = $false	
+# $BreGO = $false	
+# $CgnGO = $false	
+# $BerGO = $false	
+# $HajGO = $false	
+# $FraGO = $false	
+# $AllGO = $false	
 
-switch ($Station) {
-    "MUC" { $MucGO= $true}
-    "HAM" { $HamGO= $true}
-    "HQ"  { $HqGO= $true}
-    "BRE" { $BreGO= $true}
-    "CGN" { $CgnGO= $true}
-    "BER" { $BerGO= $true}
-    "HAJ" { $HajGO= $true}
-    "FRA" { $FraGO= $true}
-    "ALL" { $MucGO=$HamGO=$HqGO=$BreGO=$CgnGO=$BerGO=$HajGO=$FraGO = $true}
-    Default { return "Station Not Recognised" }
-}
+# switch ($Station) {
+#     "MUC" { $MucGO= $true}
+#     "HAM" { $HamGO= $true}
+#     "HQ"  { $HqGO= $true}
+#     "BRE" { $BreGO= $true}
+#     "CGN" { $CgnGO= $true}
+#     "BER" { $BerGO= $true}
+#     "HAJ" { $HajGO= $true}
+#     "FRA" { $FraGO= $true}
+#     "ALL" { $MucGO=$HamGO=$HqGO=$BreGO=$CgnGO=$BerGO=$HajGO=$FraGO = $true}
+#     Default { return "Station Not Recognised" }
+# }
 
-$Reverse
+# $Reverse
+
+# Get-Mailbox -ResultSize 5000 | where {$_.EmailADdresses -like "ber*"} | Select DisplayName, RecipientTypeEmailAddresses | Out-File ber.txt
+# Get-Recipient -ResultSize 5000 | where {$_.EmailADdresses -like "*ber*"} | Select DisplayName, EmailAddresses | Out-File ber.txt
+
+
--- dhcp/dhcpBackup.ps1
+++ dhcp/dhcpBackup.ps1
@@ -1,3 +1,62 @@
 #Back Up for Server DHCP
 
-Backup-DhcpServer -ComputerName "dcber01.intern.ahs-de.com" -Path "C:\Users\ahase\Documents\localBase\scripts\panic\dhcp\BACKUP"
+#Note this path applys to the local dirve of the DHCP server
+# Backup-DhcpServer -ComputerName "dcber01.intern.ahs-de.com" -Path "C:\Windows\system32\dhcp\manBackup"
+
+#This path confusingly refers to the local path of machine running the script. Fun I know!!!
+# $testExportFolder = Test-Path "\\dcber01.intern.ahs-de.com\C$\exportdir"
+# $testExportFolder
+# if(-Not ($testExportFolder)){
+#     mkdir "\\dcber01.intern.ahs-de.com\C$\exportdir"
+# }
+# Export-DhcpServer -ComputerName "dcber01.intern.ahs-de.com" -File "\\dcber01.intern.ahs-de.com\C$\exportdir\dhcpexport.xml"
+
+
+#list gathered from here https://cnf.ahs-de.com/pages/viewpage.action?pageId=5572026
+
+#generate list
+
+$DHCPSERVERS = Get-DhcpServerInDc
+
+foreach ($server in $DHCPSERVERS) {
+    $server.DnsName
+    $writeFlag = $true
+    #Note this path applys to the local dirve of the DHCP server
+    try{Backup-DhcpServer -ComputerName $server.DnsName -Path "C:\Windows\system32\dhcp\manBackup"}
+    catch{
+        Write-Host "Probably not working" -ForegroundColor DarkCyan
+        $writeFlag = $false
+    }
+    # This path confusingly refers to the local path of machine running the script. Fun I know!!!
+    $ExportFolder ="\\"+$server.DnsName+"\C$\exportdir"
+    $testExportFolder = Test-Path $ExportFolder
+    $testExportFolder
+    if(-Not ($testExportFolder)){
+        try{mkdir $ExportFolder}
+        catch{
+            Write-Host "Denied" -ForegroundColor Red
+            $writeFlag = $false
+        }
+    }
+    try{
+        Export-DhcpServer -ComputerName $server.DnsName -File $ExportFolder+"\dhcpexport.xml"
+    }catch{
+        Write-Host "Cannot Do this" -ForegroundColor Red
+        $writeFlag = $false
+    }
+    
+
+    #Copy Everything to here
+    if(-Not ($writeFlag)){
+        $DhcpBackupPath = "\\" + $server.DnsName + "\C$\Windows\system32\dhcp\manBackup" 
+        $DhcpBackupPath
+        $LocalBackupPath = "C:\Users\ahase\Documents\localBase\scripts\panic\dhcp\backupData\"+$server.DnsName
+        Copy-Item $DhcpBackupPath -Destination $LocalBackupPath -Recurse
+        #Copy Exports
+        $DhcpExportsXmlFile = $ExportFolder+"\dhcpexport.xml"
+        $LocalDhcpExportsXmlFile = "C:\Users\ahase\Documents\localBase\scripts\panic\dhcp\backupData\exports"+$server.DnsName
+        Copy-Item -Path $DhcpExportsXmlFile -Destination $LocalDhcpExportsXmlFile
+    }
+  
+
+}
